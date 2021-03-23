1bfe95edf60a3f45246a7fd24b946be0d7c3599f
feeldiac <feeldiac@gmail.com>
2021-03-23 03:22:39 -0500

    Adding reactive div

--- colorPicker/js/color.js
+++ colorPicker/js/color.js
@@ -6,6 +6,7 @@ color.addEventListener("input", () => {
     const value = color.value;
     // console.log(value);
     salida.innerHTML = value;
-
+    salida.style.background = value;
+    salida.style.color = "#fff";
 })
 
