5ecc124c69ce2e6e52473edfab6f73e2f226e86b
feeldiac <feeldiac@gmail.com>
2021-03-26 17:39:23 -0500

    Styling details, adding onkeyup event and update function

---
+++ LiveCodeEditor/js/app.dev.js
@@ -0,0 +1,8 @@
+const text = document.querySelector("#text");
+const view = document.querySelector("#view");
+
+const update = () => {
+
+    view.srcdoc = text.value;
+    console.log(text.value)
+}
