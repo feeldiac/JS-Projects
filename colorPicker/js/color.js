83a5b75069119287cf4a40fef2cc4c5ea658130d
feeldiac <feeldiac@gmail.com>
2021-04-11 23:14:10 -0500

    Adding gradient angle functionality

--- colorPicker/js/color.js
+++ colorPicker/js/color.js
@@ -2,6 +2,7 @@ const color = document.querySelector("#color");
 const salida = document.querySelector("#salida");
 const color2 = document.querySelector("#color2");
 const salida2 = document.querySelector("#salida2");
+const angle = document.getElementById("angle");
 const msg = document.getElementById("msg");
 const btn = document.getElementById("btn");
 
@@ -11,8 +12,6 @@ color.addEventListener("input", () => {
     salida.innerHTML = value;
     salida.style.background = value;
     salida.style.color = "#fff";
-
-    return value;
 })
 
 color2.addEventListener("input", () => {
@@ -20,12 +19,10 @@ color2.addEventListener("input", () => {
     salida2.innerHTML = value2;
     salida2.style.background = value2;
     salida2.style.color = "#fff";
-
-    return value2;
 })
 
 btn.addEventListener("click", function () {
-    document.body.style.background = `linear-gradient(to right, ${color.value}, ${color2.value})`;
+    document.body.style.background = `linear-gradient(${angle.value}deg, ${color.value}, ${color2.value})`;
     msg.textContent = document.body.style.background;
   });
 
