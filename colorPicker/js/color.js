ca5957f8fbc1d5e609de7c9dde245ba4871dd9d5
feeldiac <feeldiac@gmail.com>
2021-04-12 21:44:19 -0500

    Design changes, deleting button, adding more reactivity

--- colorPicker/js/color.js
+++ colorPicker/js/color.js
@@ -3,10 +3,13 @@ const salida = document.querySelector("#salida");
 const color2 = document.querySelector("#color2");
 const salida2 = document.querySelector("#salida2");
 const angle = document.getElementById("angle");
-const msg = document.getElementById("msg");
-const btn = document.getElementById("btn");
-const stop1 = document.getElementById("stop1");
-const stop2 = document.getElementById("stop2");
+const show_angle = document.getElementById("show-angle");
+const msg = document.getElementById("msg"); 
+let stop1 = document.getElementById("stop1");
+let stop2 = document.getElementById("stop2");
+const defaultValues = [0, 100, ""];
+// stop1 = "";
+// stop2 = "";
 
 //Reacciona a los cambios de color en tiempo real
 color.addEventListener("input", () => {
@@ -14,6 +17,8 @@ color.addEventListener("input", () => {
     salida.innerHTML = value;
     salida.style.background = value;
     salida.style.color = "#fff";
+    document.body.style.background = `linear-gradient(${angle.value}deg, ${color.value} ${stop1.value}%, ${color2.value} ${stop2.value}%)`;
+    msg.textContent = document.body.style.background;
 })
 
 color2.addEventListener("input", () => {
@@ -21,12 +26,16 @@ color2.addEventListener("input", () => {
     salida2.innerHTML = value2;
     salida2.style.background = value2;
     salida2.style.color = "#fff";
+    document.body.style.background = `linear-gradient(${angle.value}deg, ${color.value} ${stop1.value}%, ${color2.value} ${stop2.value}%)`;
+    msg.textContent = document.body.style.background;
+})
+
+angle.addEventListener("input", () => {
+    show_angle.innerHTML = angle.value;
+    document.body.style.background = `linear-gradient(${angle.value}deg, ${color.value} ${stop1.value}%, ${color2.value} ${stop2.value}%)`;
+    msg.textContent = document.body.style.background;
 })
 
 
-btn.addEventListener("click", function () {
-    document.body.style.background = `linear-gradient(${angle.value}deg, ${color.value} ${stop1.value}%, ${color2.value} ${stop2.value}%)`;
-    msg.textContent = document.body.style.background;
-  });
 
 
